4d5dc352aef6a638427d45327d1448d9f1397561
Samridhi Bhardwaj <sbhardwaj_be24@thapar.edu>
2026-06-23 21:52:02 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class LogisticRegressionModel(nn.Module):
+
+    def __init__(self, input_dim):
+        super().__init__()
+
+        self.linear = nn.Linear(input_dim, 1)
+
+    def forward(self, x):
+        return torch.sigmoid(self.linear(x))
